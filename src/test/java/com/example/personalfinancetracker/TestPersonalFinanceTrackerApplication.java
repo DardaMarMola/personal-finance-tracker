8ef00cdfbf7a84fc9f051f8ae16a9de47b51bb99
Mehdi <mehdi.shabanzadeh86@gmail.com>
2026-02-08 12:11:59 +0330

    init finance tracker project

---
+++ src/test/java/com/example/personalfinancetracker/TestPersonalFinanceTrackerApplication.java
@@ -0,0 +1,11 @@
+package com.example.personalfinancetracker;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestPersonalFinanceTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.from(PersonalFinanceTrackerApplication::main).with(TestcontainersConfiguration.class).run(args);
+    }
+
+}
